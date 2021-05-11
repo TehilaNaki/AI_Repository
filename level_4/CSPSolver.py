9fff637de85e0280b52c195eded7b2109efe2f45
Tehila Naki <72945182+TehilaNaki@users.noreply.github.com>
2021-05-11 13:18:00 +0300

    level_4

--- level_4/CSPSolver.py
+++ level_4/CSPSolver.py
@@ -1,5 +1,6 @@
 import CSProblem
 import copy
+import collections
 
 
 def solve(n):
@@ -7,14 +8,14 @@ def solve(n):
 
 
 def backtrack(p):
-    var = next_var(p, MRV=False)  # MRV-Most constrained variable or Minimum Remaining Values
+    var = next_var(p, MRV=True)  # MRV-Most constrained variable or Minimum Remaining Values
     if var == None:
         return p
-    dom = sorted_domain(p, var, LCV=False)  # LCV-least constraining value
+    dom = sorted_domain(p, var, LCV=True)  # LCV-least constraining value
     for i in dom:
         bu = copy.deepcopy(p)
         CSProblem.assign_val(bu, var, i)
-        propagate_constraints(bu, var)  #
+        propagate_constraints(bu, var)  # delete the optione after choose i
         bu = backtrack(bu)
         if CSProblem.is_solved(bu):
             return bu
@@ -24,15 +25,34 @@ def backtrack(p):
 def sorted_domain(p, var, LCV=True):
     if LCV == False:
         return CSProblem.domain(p, var)
+    dom = [[0,0]]
+    p1 = []
+    # for each option in the domain of var
+    for item in p[1][var]:
+        p1 = copy.deepcopy(p)
+        CSProblem.assign_val(p1, var, item)
+        dif = difference(p1, var, item)
+        l=(item,dif)
+       # dom.insert([l])
+    sort = sorted(dom, key=second)
+    return [i[0] for i in sort]
 
-    # your code here
 
-    return None
+def difference(p1, var, item):
+    before = num_of_del_vals(p1)
+    propagate_constraints(p1, var)  # delete the optione after choose i
+    after = num_of_del_vals(p1)
+    return before - after
 
 
+def second(p):
+    return p[1]
+
+
+# return the num of options deleted from domain
 def num_of_del_vals(l):
     # l=[problem, the variable, the val. assigned to the var.]
-    # returns the num. of vals. erased from vars domains after assigning x to v
+    # returns the num. of values. erased from vars domains after assigning x to v
     count = 0
     for inf_v in CSProblem.list_of_influenced_vars(l[0], l[1]):
         for i in CSProblem.domain(l[0], inf_v):
@@ -53,10 +73,9 @@ def next_var(p, MRV=True):
             return None
         else:
             return v[0]
-
-    # your code here
-
-    return None
+    lengs = [len(item) for item in p[1]]
+    index = lengs.index(min(lengs))
+    return index
 
 
 def propagate_constraints(p, v):
@@ -66,7 +85,4 @@ def propagate_constraints(p, v):
                 CSProblem.erase_from_domain(p, i, x)
 
 
-solve(10)
-
-
-
+solve(5)
--- level_4/CSProblem.py
+++ level_4/CSProblem.py
@@ -10,9 +10,10 @@
 
 def create(n):  # the board size is nXn
     d = []  # d is the domains
+    # puts the options in the domain
     for i in range(n):
         d += [list(range(n))]
-    return [[-1] * n, d]
+    return [[-1] * n, d]  #[[-1,-1,-1],[[1,2,3],[1,2,3],[1,2,3]]]
 
 
 def domain(problem, v):  # problem is the state, v is a variable (a row)
@@ -48,21 +49,21 @@ def get_list_of_free_vars(problem):
             l += [i]
     return l
 
-
+# check if the problem is solve
 def is_solved(problem):
-    # Returns True iff the problem is solved
+    # Returns True if the problem is solved
     for i in range(len(problem[0])):
         if problem[0][i] == -1:
             return False
     return True
 
-
+# check if the state is ok
 def is_consistent(problem, v1, v2, x1, x2):
-    # Returns True iff v1=x1 and v2=x2 is consistent with all constraints
+    # Returns True if v1=x1 and v2=x2 is consistent with all constraints
     return x1 != x2 and abs(v1 - v2) != abs(x1 - x2)  # x1!=x2 - not on the same col.
     # abs(v1-v2)!=abs(x1-x2) - not on the same diagonal
 
-
+# delete v from the free vars list
 def list_of_influenced_vars(problem, v):
     # Returns a list of vars. whose domain will be
     # influenced by assigning a val. to v
@@ -71,7 +72,7 @@ def list_of_influenced_vars(problem, v):
         l.remove(v)
     return l
 
-
+# print the board
 def present(problem):
     print()
     for r in range(len(problem[0])):
